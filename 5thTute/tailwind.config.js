c7600936494e35052b37099f7c77e740007f09ba
RahulHomePersonal <ra10011996@gmail.com>
2023-03-10 22:49:30 +0530

    till project 1

---
+++ 5thTute/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize: {
+        "10xl": ["9rem", { lineHeight: "2" }],
+      },
+    },
+  },
+  plugins: [],
+};
